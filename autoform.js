a823390eb3ac5f1645553625c8383aaf5f87e797
nil-bora <brdnlsrg@gmail.com>
2015-09-01 17:11:15 +0300

    Update autoform.js

--- autoform.js
+++ autoform.js
@@ -18,3 +18,27 @@ var autoForm = function(params) {
 	
 };
 autoForm(params);
+
+ function autoForm(args) {
+        var defaults = {
+            text: 'asdadsSD',
+            email: 'test@test.com',
+            tel: '+380931111111'
+        };
+
+        args = $.extend({}, defaults, args);
+
+        $('textarea').val(args.text);
+        $('input').each(function (i) {
+
+            if (args[$(this).attr('type')]) {
+                $(this).val(args[$(this).attr('type')]);
+
+            } else {
+                $(this).val(args.text);
+            }
+        });
+    }
+
+    autoForm({text: 'RAQ SOOQA'});
+

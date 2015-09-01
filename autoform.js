128aca014216d9c2b3641c6980436e3b8c32c290
nil-bora <brdnlsrg@gmail.com>
2015-09-01 17:43:08 +0300

    Update autoform.js

--- autoform.js
+++ autoform.js
@@ -1,21 +1,53 @@
- function autoForm(args) {
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+<input type="tel"/><br/><br/>
+<input type="email"/><br/><br/>
+<input type="text" name="email"/><br/><br/>
+<textarea></textarea>
+
+
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script>
+
+    function autoForm(args, parent) {
         var defaults = {
-            text: 'asdadsSD',
+            default: 'asdadsSD',
             email: 'test@test.com',
             tel: '+380931111111'
         };
 
         args = $.extend({}, defaults, args);
+        parent = typeof parent !== 'undefined' ? parent : 'html';
 
-        $('textarea').val(args.text);
-        $('input').each(function (i) {
+        var input = $(parent).find('input'),
+            textarea = $(parent).find('textarea');
 
-            if (args[$(this).attr('type')]) {
-                $(this).val(args[$(this).attr('type')]);
-            } else {
-                $(this).val(args.text);
-            }
+        textarea.val(args.default);
+        input.each(function () {
+
+            var $this = $(this),
+            type = $this.attr('type'),
+            name = $this.attr('name');
+
+            if (args[type])
+                $this.val(args[type]);
+            else if (args[name])
+                $this.val(args[name]);
+            else
+                $this.val(args.default);
+
         });
     }
 
-    autoForm({text: 'RAQ SOOQA'});
+    autoForm({
+        default: 'RAQ SOOQA'
+    }, 'body');
+</script>
+</body>
+</html>
